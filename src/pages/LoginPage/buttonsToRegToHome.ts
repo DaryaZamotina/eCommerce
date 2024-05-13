eb906f8a0ea43a96eb511664a0ec299072648c76
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-05-13 17:12:05 +0300

    Rss ecomm 2 01 (#62)
    * feat: Add loginForm folder and obtain access token
    
    * feat: add localStorage for accessToken
    
    * feat: add function getAccessToken
    
    * feat: obtain userInfo
    
    * feat: obtain data of user
    
    * fix: correct name of keyOfAPIClient
    
    * feat: add two inputs for login
    
    * feat: login user
    
    * feat: amend login func
    
    * feat: add new file and change logic of code
    
    * fix: amend code of putting button login
    
    * feat: add button to registration
    
    * feat: add button to main page
    
    * feat: add error message
    
    * feat: add error message and fix bug for direct to main page
    
    * feat: receive token
    
    * fix: change name of loginForm file and correct form tag name
    
    * feat: amend tag creation
    
    * fix: amend logic of tag creation for buttonLogin

---
+++ src/pages/LoginPage/buttonsToRegToHome.ts
@@ -0,0 +1,30 @@
+import RegistrationForm from "../../pages/Registration/registrationForm";
+
+export function moveToRegistration() {
+    const buttonRegistration = document.getElementById("buttonRegistration");
+
+    buttonRegistration.addEventListener("click", function() {
+        const loginFormDiv = document.getElementById("loginForm");
+        loginFormDiv.remove();
+
+        const buttonsWrapper = document.getElementById("buttonsWrapper");
+        buttonsWrapper.remove();
+
+        const registrationFormDiv = new RegistrationForm();
+        registrationFormDiv.createRegistrationForm();
+    });
+}
+
+export function moveToMainPage() {
+    const buttonToMainPage = document.getElementById("buttonToMainPage");
+
+    buttonToMainPage.addEventListener("click", function() {
+        directMoveToMainPage()
+});
+}
+
+export function directMoveToMainPage() {
+    const body = document.getElementById("body");
+    body.innerHTML = "";
+    body.textContent = "MAIN PAGE";
+}
---
+++ src/pages/LoginPage/loginForm.ts
@@ -0,0 +1,72 @@
+import TagCreator from "../../module/tagCreator";
+import CreateInputForForm from "../creatorInputForForm";
+import InputsForFormLogin from "../../Helpers/Inputs/InputsForFormLogin";
+
+export default class LoginForm {
+    constructor() {}
+  
+    public createLoginForm() {
+      const loginForm = new TagCreator(
+        "form",
+        "loginForm",
+        "loginForm",
+        "body",
+      );
+      loginForm.createAndAppend();
+
+      const buttonsWrapper = new TagCreator(
+        "form",
+        "buttonsWrapper",
+        "buttonsWrapper",
+        "body",
+      );
+      buttonsWrapper.createAndAppend();
+
+      const inputs = new CreateInputForForm(InputsForFormLogin);
+      inputs.createAndAppend();
+
+      /*const buttonLogin = new TagCreator(
+        "button",
+        "buttonLogin",
+        "buttonLogin",
+        "buttonsWrapper",
+        "Login"
+      )
+      buttonLogin.createAndAppend(); */
+
+      const buttonLogin: HTMLButtonElement = document.createElement('button');
+      buttonLogin.className = 'buttonLogin';
+      buttonLogin.id = 'buttonLogin';
+      buttonLogin.textContent = "Login";
+
+      const body = document.getElementById("body");
+      body.append(buttonLogin);
+
+      const buttonRegistration = new TagCreator(
+        "button",
+        "buttonRegistration",
+        "buttonRegistration",
+        "buttonsWrapper",
+        "Go to Registration"
+      )
+      buttonRegistration.createAndAppend();
+
+      const buttonToMainPage = new TagCreator(
+        "button",
+        "buttonToMainPage",
+        "buttonToMainPage",
+        "buttonsWrapper",
+        "Home"
+      )
+      buttonToMainPage.createAndAppend();
+
+      const messageAboutError = new TagCreator(
+        "div",
+        "messageAboutError",
+        "messageAboutError",
+        "buttonsWrapper",
+        ""
+      )
+      messageAboutError.createAndAppend();
+    }
+  }

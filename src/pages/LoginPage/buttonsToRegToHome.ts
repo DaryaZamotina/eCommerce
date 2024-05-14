01dc64a13a27d76d5979b12d16ff21766f4b4c98
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-05-14 21:15:08 +0300

    feat: add registeration info to ecomm (#64)

--- src/pages/LoginPage/buttonsToRegToHome.ts
+++ src/pages/LoginPage/buttonsToRegToHome.ts
@@ -1,30 +1,33 @@
-import RegistrationForm from "../../pages/Registration/registrationForm";
+import RegistrationForm from '../../pages/Registration/registrationForm';
+import '../../../public/assets/css/body.css';
+import { receiveAccessToken } from './loginGetToken';
 
 export function moveToRegistration() {
-    const buttonRegistration = document.getElementById("buttonRegistration");
+  const buttonRegistration = document.getElementById('buttonRegistration');
 
-    buttonRegistration.addEventListener("click", function() {
-        const loginFormDiv = document.getElementById("loginForm");
-        loginFormDiv.remove();
+  buttonRegistration.addEventListener('click', function () {
+    const loginFormDiv = document.getElementById('loginForm');
+    loginFormDiv.remove();
 
-        const buttonsWrapper = document.getElementById("buttonsWrapper");
-        buttonsWrapper.remove();
+    const buttonsWrapper = document.getElementById('buttonsWrapper');
+    buttonsWrapper.remove();
 
-        const registrationFormDiv = new RegistrationForm();
-        registrationFormDiv.createRegistrationForm();
-    });
+    const registrationFormDiv = new RegistrationForm();
+    registrationFormDiv.createRegistrationForm();
+    receiveAccessToken();
+  });
 }
 
 export function moveToMainPage() {
-    const buttonToMainPage = document.getElementById("buttonToMainPage");
+  const buttonToMainPage = document.getElementById('buttonToMainPage');
 
-    buttonToMainPage.addEventListener("click", function() {
-        directMoveToMainPage()
-});
+  buttonToMainPage.addEventListener('click', function () {
+    directMoveToMainPage();
+  });
 }
 
 export function directMoveToMainPage() {
-    const body = document.getElementById("body");
-    body.innerHTML = "";
-    body.textContent = "MAIN PAGE";
+  const body = document.getElementById('body');
+  body.innerHTML = '';
+  body.textContent = 'MAIN PAGE';
 }
--- src/pages/Registration/registrationForm.ts
+++ src/pages/Registration/registrationForm.ts
@@ -1,7 +1,9 @@
-import TagCreator from "../../module/tagCreator";
-import CreateInputForForm from "../creatorInputForForm";
-import InputsForFormRegistration from "../../Helpers/Inputs/InputsForFormRegistration";
-import "../../../public/assets/css/body.css";
+import TagCreator from '../../module/tagCreator';
+import CreateInputForForm from '../creatorInputForForm';
+import InputsForFormRegistration from '../../Helpers/Inputs/InputsForFormRegistration';
+import '../../../public/assets/css/body.css';
+import { forwardRegDatatoServer } from '../Registration/sendDataToEcomm';
+import { receiveAccessToken } from '../LoginPage/loginGetToken';
 import "../../../public/assets/css/registrationForm.css";
 
 export default class RegistrationForm {
@@ -9,10 +11,10 @@ export default class RegistrationForm {
 
   public createRegistrationForm() {
     const form = new TagCreator(
-      "from",
-      "registrationForm",
-      "registrationForm",
-      "body",
+      'from',
+      'registrationForm',
+      'registrationForm',
+      'body',
     );
     form.createAndAppend();
 
@@ -27,5 +29,20 @@ export default class RegistrationForm {
 
     const inputs = new CreateInputForForm(InputsForFormRegistration);
     inputs.createAndAppend();
+
+    const buttonToSendRegDataToServer: HTMLButtonElement =
+      document.createElement('button');
+    buttonToSendRegDataToServer.className = 'buttonToSendRegDataToServer';
+    buttonToSendRegDataToServer.id = 'buttonToSendRegDataToServer';
+    buttonToSendRegDataToServer.textContent = 'Send Data to Ecomm';
+    buttonToSendRegDataToServer.type = 'button';
+
+    const body = document.getElementById('body');
+    body.append(buttonToSendRegDataToServer);
+
+    buttonToSendRegDataToServer.addEventListener('click', function () {
+      const tokenForAuth = localStorage.getItem('access_token_auth');
+      forwardRegDatatoServer(tokenForAuth);
+    });
   }
 }

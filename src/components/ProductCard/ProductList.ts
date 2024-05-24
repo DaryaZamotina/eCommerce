a7469ff933806a609b7d7fcfaf2a714a9dc7b53e
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-05-24 22:20:36 +0300

    feat: add goods info to goods div

--- src/components/ProductCard/ProductList.ts
+++ src/components/ProductCard/ProductList.ts
@@ -3,10 +3,25 @@ import TagCreator from '../../module/tagCreator';
 //import InputsForFormLogin from '../../Helpers/Inputs/InputsForFormLogin';
 import '../../../public/assets/css/body.css';
 import '../../../public/assets/css/button.css';
+import '../../../public/assets/css/products.css';
 import { getProductsListInfoFromEcomm } from './getProductDataFromEcomm';
+import IResult from './InterfaceProduct';
 //import HomePage from '../../pages/Home/homePage';
 
+
+export function createProductsList(n: number, obj: Array <IResult>) {
+  for (let i = 0; i < n; i++) {
+    const productCardInfoContainer = document.createElement("div");
+    productCardInfoContainer.className = "productCardInfoContainer";
+    productCardInfoContainer.textContent = JSON.stringify(obj[i]);
+
+    const homeSection = document.getElementById("homeSection");
+    homeSection.append(productCardInfoContainer);  
+  }
+}
+
 export default class Products {
+
   /*private id: string;
   // private form: 'prod';
 
@@ -33,4 +48,4 @@ export default class Products {
     );
     productListTitle.createAndAppend();
   }*/
-}
+} 
--- src/components/ProductCard/getProductDataFromEcomm.ts
+++ src/components/ProductCard/getProductDataFromEcomm.ts
@@ -1,4 +1,5 @@
 import { receiveAnonymusAccessToken } from '../../pages/Home/anonymusSessionToken';
+import { createProductsList } from './ProductList';
 
 export function getProductsListInfoFromEcomm(token: string) {
   const link =
@@ -23,11 +24,15 @@ export function getProductsListInfoFromEcomm(token: string) {
 
       const infoJSON = JSON.parse(info);
 
-      const homeSection = document.getElementById('homeSection');
-      homeSection.textContent = infoJSON.results;
+      //const homeSection = document.getElementById('homeSection');
+      //homeSection.textContent = infoJSON.results;
 
       console.log('goods = ' + info);
-      console.log("infoJSON.results = " + infoJSON.results);
+      console.log('infoJSON.results = ' + infoJSON.results);
+      const numberOfGoods = infoJSON.results.length;
+      console.log("numberOfGoods = " + numberOfGoods);
+      createProductsList(numberOfGoods, infoJSON.results);
+
       /*console.log('refresh_token_for_user = ' + infoJSON.refresh_token);
         localStorage.setItem('access_token_for_user', infoJSON.access_token);
         localStorage.setItem('refresh_token_for_user', infoJSON.refresh_token);*/

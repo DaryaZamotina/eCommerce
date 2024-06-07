39a6ca84862a67a9f871ccea0155e5143a5da914
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-06-07 21:12:56 +0300

    feat: add btn to cart to product list

---
+++ src/components/ProductCard/createBtnToCart.ts
@@ -0,0 +1,35 @@
+
+import TagCreator from "../../module/tagCreator";
+import '../../../public/assets/css/products.css';
+import { createCart } from "../../pages/Cart/createCart";
+
+export function createButtonToCart(resultId: string) {
+
+    const container = document.getElementById(`catalogContainer_${resultId}`);
+
+const btnToCart  = document.createElement("button");
+btnToCart.className = "btnToCart";
+btnToCart.id = "btnToCart";
+container.append(btnToCart);
+
+   /* const btnToCart = new TagCreator(
+        'button',
+        'btnToCart',
+        'btnToCart',
+        `catalogContainer_${resultId}`,
+        ``,
+      );
+    btnToCart.createAndAppend(); */
+
+   //const button = <HTMLButtonElement>document.getElementById("btnToCart");
+
+   btnToCart.addEventListener("click", (e) => {
+    //if(this==e.target)
+    e.preventDefault();
+    btnToCart.style.backgroundColor = 'red';
+    //createCart();
+   // e.stopImmediatePropagation();
+    e.stopPropagation();
+   //this.style.backgroundColor = 'red';
+   })
+}
---
+++ src/pages/Cart/createCart.ts
@@ -0,0 +1,14 @@
+/*
+curl https://api.{region}.commercetools.com/{projectKey}/carts -i \
+--header 'Authorization: Bearer ${BEARER_TOKEN}' \
+--header 'Content-Type: application/json' \
+--data-binary @- << DATA 
+{
+  "currency" : "EUR"
+}
+DATA
+*/
+
+export function createCart() {
+    
+}

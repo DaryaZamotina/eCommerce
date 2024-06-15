cf8ec66488b170b750dbf794cef343ef5bebddec
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-06-15 14:07:02 +0300

    fix: remove new fetch of products if exists

--- src/pages/Home/anonymusSessionToken.ts
+++ src/pages/Home/anonymusSessionToken.ts
@@ -63,9 +63,9 @@ export function receiveAnonymusAccessToken() {
       const jsonToken = JSON.parse(output);
       const anonymAccessToken = jsonToken.access_token;
       localStorage.setItem('anonym_access_token', anonymAccessToken);
-      let tokenForGoods = localStorage.getItem('anonym_access_token');
-      getProductsListInfoFromEcomm(tokenForGoods);
-      console.log('anonym_access_token = ' + anonymAccessToken);
+  //    let tokenForGoods = localStorage.getItem('anonym_access_token');
+  //    getProductsListInfoFromEcomm(tokenForGoods);
+   //   console.log('anonym_access_token = ' + anonymAccessToken);
       return output;
     })
     .catch((err) => console.log(err));

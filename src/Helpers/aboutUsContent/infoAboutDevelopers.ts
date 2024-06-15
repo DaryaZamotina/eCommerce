0294cffd0bf9e16f91a1759fa632882f46937441
Alison WEB <AlisonWEB@yandex.ru>
2024-06-15 19:03:54 +0300

    fix: path to rss logo for deploy, change some text content about us

--- src/Helpers/aboutUsContent/infoAboutDevelopers.ts
+++ src/Helpers/aboutUsContent/infoAboutDevelopers.ts
@@ -6,9 +6,9 @@ export default [
     city: 'Saint Petersburg',
     photo: './images/dasha.jpg',
     about:
-      'The team lead and PR manager of our project. Solving arising problems with optimism and humour she tries to maintain positive atmosphere in the command.  She makes interaction between E-commerce tools and our programming code, as well as between different parts of the program in order to put scattered parts of the code into one well organised mechanism.',
+      'The team lead and PR manager of our project. Solving arising problems with optimism and humour she tries to maintain positive atmosphere in the command. She makes interaction between E-commerce tools and our programming code, as well as between different parts of the program in order to put scattered parts of the code into one well organised mechanism. In real life Darya works as a trademark attorney, raising three children and being a master of the best cat in the world!',
     areas:
-      'Login Page | Product Page | Profile User Page | HTTP requests to API',
+      'Login Page | Product Page | Profile User Page | Cart  Page | HTTP requests to API',
   },
   {
     name: 'Vladimir',
@@ -17,9 +17,9 @@ export default [
     city: 'Samara',
     photo: './images/vladimir.jpg',
     about:
-      'The tech lead of our company. His programming skills and competent coding manner make our project readable, usable and maintainable. Usage of advanced technologies and interesting programming decisions help to make our project the best one! He also completes all work exactly on time and according to the task plan that allows us to be confident in quality and relevance of his work.',
+      "The tech lead of our company. His programming skills and competent coding manner make our project readable, usable and maintainable. Usage of advanced technologies and interesting programming decisions help to make our project the best one! He also completes all work exactly on time and according to the task plan that allows us to be confident in quality and relevance of his work. Driven by his passion for science, Vladimir earned a Master's degree in laser physics and spectroscopy from Samara National Research University and works in the field of education.",
     areas:
-      'Registration Page | Catalog | Development Environment Configuration',
+      'Registration Page | Catalog | Cart  Page | Development Environment Configuration',
   },
   {
     name: 'Alice',
@@ -28,8 +28,8 @@ export default [
     city: 'Rostov-on-Don',
     photo: './images/alisa.jpg',
     about:
-      'The main economic specialist and creative designer of our team! Only for her effort our ECommerce project works like Swiss watches! Also her attention to details and pursuit for excellence do our project better and better with each code reveiew. Idea of our best furniture shop also belongs to her as well as logo you look for when visit our site. ',
+      'The main economic specialist and creative designer of our team! Her attention to details and pursuit for excellence do our project better and better with each code reveiew. Idea of our best furniture shop also belongs to her as well as logo you look for when visit our site. Alisa holds a postgraduate degree from the Rostov Conservatory, where she also worked as an accompanist prior to taking maternity leave.',
     areas:
-      'Header | Routing | About Us Page | Adding goods to the CommerceTools | Readme.md',
+      'Header | Routing | About Us Page | Pagination | Adding goods to the CommerceTools | Readme.md',
   },
 ];

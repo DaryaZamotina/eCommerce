5c1230f95304247c6b4c60e6e0b3327809124886
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-06-20 22:20:45 +0300

    fix: change info

--- src/Helpers/spinner.ts
+++ src/Helpers/spinner.ts
@@ -14,6 +14,6 @@ export default function spinner(value: boolean) {
     spinnerBody.createAndAppend();
     spinner.createAndAppend();
   } else {
-    document.getElementById('spinnerBody').remove();
+    document.getElementById('spinnerBody')?.remove();
   }
 }
